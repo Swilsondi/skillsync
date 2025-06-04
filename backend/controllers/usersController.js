4001c30462ecfe91343a7f3f2dc74dcc9cb805a3
Swilsondi <165120193+Swilsondi@users.noreply.github.com>
2025-06-03 20:42:57 -0400

    Adds user management functionality to the controller
    Implements user-related operations including fetching all users, creating a new user, and handling user retrieval, editing, and deletion in the usersController.
    
    Updates the usersRouter to use the new controller methods, improving code organization and maintainability.
    
    Enhances the user management features for better API usability.

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,3 +1,4 @@
+//Get users
 exports.getAllUsers = async (req, res, next) => {
     try {
         const db = req.app.locals.db; //You grab the users collection from the database.
@@ -12,4 +13,46 @@ exports.getAllUsers = async (req, res, next) => {
     }
 };
 
+//Create User
+exports.createUser = async (req, res, next) => {
+    try{
+        const db = req.app.locals.db;
+        const userData = req.body;
+        const result = await db.collection('users').insertOne(userData); 
+    res.status(201).json({
+        status: 'success',
+          data: {
+    insertedId: result.insertedId,
+    message: 'User created successfully'
+  }})
+}
+catch (err){
+        next(err);
+    }
+};
 
+//Get user by ID
+const userId = (req, res) => {
+    res.status(201).json({
+        status: 'success',
+        message: 'You have successfully found user by id'
+    });
+};
+
+
+//Edit user by ID
+const editUser = (req, res) => {
+    res.status(201).json({
+        status: 'success',
+        message: 'You have successfully edited user'
+    })
+}
+
+
+// Delete User
+const deleteUser = (req, res) => {
+    res.status(201).json({
+        status: 'success',
+        message: 'You have successfully deleted user'
+    })
+}
